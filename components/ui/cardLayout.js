d199c60223c7e9b3ba2d1915f0ad7bd24781657a
trungbanh <trungbanh97@gmail.com>
2019-05-19 14:05:54 +0700

    merge code from thanh

--- components/ui/cardLayout.js
+++ components/ui/cardLayout.js
@@ -3,37 +3,37 @@ export default () => (
   <div className="container">
     <div className="row">
       <div className="col-4">
-        <a href="#" class="card">
+        <a href="#" className="card">
           <div style={{ display: 'inline-block' }} class="card-body">
             <h5 class="card-title">Card title</h5>
           </div>
           <img
             src="https://www.datocms-assets.com/2885/1542060101-nomadshare-imglogo-w-stack-graphic1200x630.png?fit=max&fm=webp&q=80&w=2000"
-            class="card-img-bottom"
+            className="card-img-bottom"
             alt="..."
           />
         </a>
       </div>
       <div className="col-4">
-        <a href="#" class="card">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+        <a href="#" className="card">
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
           </div>
           <img
             src="https://www.datocms-assets.com/2885/1542060101-nomadshare-imglogo-w-stack-graphic1200x630.png?fit=max&fm=webp&q=80&w=2000"
-            class="card-img-bottom"
+            className="card-img-bottom"
             alt="..."
           />
         </a>
       </div>
       <div className="col-4">
-        <a href="#" class="card">
+        <a href="#" className="card">
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 className="card-title">Card title</h5>
           </div>
           <img
             src="https://www.datocms-assets.com/2885/1542060101-nomadshare-imglogo-w-stack-graphic1200x630.png?fit=max&fm=webp&q=80&w=2000"
-            class="card-img-bottom"
+            className="card-img-bottom"
             alt="..."
           />
         </a>
